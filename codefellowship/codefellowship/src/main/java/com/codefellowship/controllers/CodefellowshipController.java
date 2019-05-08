50b0ca7aa89b3b1549bf990f8a9a9499c24f7d2a
Merry Cimakasky <mkcimakasky@gmail.com>
2019-05-08 09:38:49 -0700

    start day2

--- codefellowship/codefellowship/src/main/java/com/codefellowship/controllers/CodefellowshipController.java
+++ codefellowship/codefellowship/src/main/java/com/codefellowship/controllers/CodefellowshipController.java
@@ -3,6 +3,8 @@ package com.codefellowship.controllers;
 import com.codefellowship.database.CodefellowshipUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +15,10 @@ public class CodefellowshipController {
     CodefellowshipUserRepository userRepo;
 
     //base route with splash page and basic info
+    @GetMapping("")
+    public String splashPage(Model model) {
+        return "splashpage";
+    }
 
     //single account info page
 
