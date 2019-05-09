40f42219f61f8f24c0ab7e48f8a4829e3b7c79ab
Merry Cimakasky <mkcimakasky@gmail.com>
2019-05-09 11:31:10 -0700

    newbranch

---
+++ codefellowship/codefellowship/src/main/java/com/codefellowship/database/Followers.java
@@ -0,0 +1,19 @@
+package com.codefellowship.database;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Followers {
+    @Id
+    @GeneratedValue
+    public long id;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    public CodefellowshipUser fromUser;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    public CodefellowshipUser toUser;
+
+    public Date followedOn;
+}
